45d098a2aca9b2397d90c694492e850849e09aa5
lenny <lenny.worsono@meltwater.org>
2012-04-05 00:29:19 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'fecce80cc1fbe3a32edd50bb4ed41eb58c6673be11479cdc0324af371300e6983cc81e3017c06a869a391d7fda65623a9486ab89f1447948054bdbde8d83c2da'
